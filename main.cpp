ea64b39305c2cc8f3461df54e8d76fc830ad4ee6
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-11-15 16:09:47 +0000

    hepta

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "mbed.h"
+#include "HEPTA_EPS.h"
+#include "HEPTA_SENSOR.h"
+ 
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_EPS eps(p16,p26);
+HEPTA_SENSOR sensor(p17,
+                  p28,p27,0x19,0x69,0x13,
+                  p13, p14,p25,p24);
 
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main()
+{
+    float ax,ay,az;
+    for(int i = 0; i<50; i++) {
+        sensor.sen_acc(&ax,&ay,&az);
+        pc.printf("acc : %f,%f,%f\r\n",ax,ay,az);
+        wait(1.0);
+    }
 }
